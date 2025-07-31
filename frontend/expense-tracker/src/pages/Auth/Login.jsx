7ac09004283810966b67d52bdf435db4ea5e5100
Eric <kaiguaeric@gmail.com>
2025-07-31 07:39:48 +0300

    Trying new fetch login

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -37,7 +37,7 @@ const Login = () =>{
     }
 
         try {
-            const response = await fetch('http://localhost:3000/login', { 
+            const response = await fetch('https://expense-tracker-fdez.onrender.com', { 
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
